2536ebd9dfb03c0213e1905ecaf27968f05834f2
Grace Colette <ilovegrace09@gmail.com>
2019-06-19 21:43:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 end
-
+require pry
 def batch_badge_creator(attendees)
   attendees.collect {|attendee| "Hello, my name is #{attendee}."}
 end
